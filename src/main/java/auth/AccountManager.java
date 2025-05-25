79ebc10e0a0be92aed7ba44c82112761ce1779fb
Seweryn Ziętko <sewezietko@gmail.com>
2025-05-25 19:40:47 +0200

    dom

--- src/main/java/auth/AccountManager.java
+++ src/main/java/auth/AccountManager.java
@@ -1,24 +1,59 @@
 package auth;
 
 import database.DatabaseConnection;
+import org.mindrot.jbcrypt.BCrypt;
+
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class AccountManager {
     private DatabaseConnection dbc;
 
+    public AccountManager(DatabaseConnection dbc) {
+        this.dbc = dbc;
+    }
+
     public boolean register(String login, String password) {
-        PreparedStatement ps = dbc.getConnection().prepareStatement("");
+        String hashed_password = BCrypt.hashpw(password, BCrypt.gensalt());
+        try {
+            PreparedStatement ps = dbc.getConnection().prepareStatement("INSERT INTO accounts (name, password) VALUES (?, ?)");
+            ps.setString(1, login);
+            ps.setString(2, hashed_password);
+            ps.executeUpdate();
+            return true;
+        } catch (SQLException e) {
+            System.out.println("Błąd przy rejestracji: " + e.getMessage());
+        }
         return false;
     }
-    public boolean authenticate(Account account){
-
+    public boolean authenticate(String login, String password){
+        try {
+            PreparedStatement ps = dbc.getConnection().prepareStatement("SELECT password FROM accounts WHERE name = ?");
+            ps.setString(1,login);
+            ResultSet rs = ps.executeQuery();
+            if(rs.next()){
+                String hashed_password = rs.getString("password");
+                return BCrypt.checkpw(password, hashed_password);
+            }
+        } catch (SQLException e) {
+            System.out.println("Błąd przy weryfikacji: " + e.getMessage());
+        }
         return false;
     }
 
-    public Account getAccount(){
-
-        return new Account(1,"");
+    public Account getAccount(String login) {
+        try {
+            PreparedStatement ps = dbc.getConnection().prepareStatement("SELECT id, name FROM accounts WHERE name = ?");
+            ps.setString(1, login);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                return new Account(rs.getInt("id"), rs.getString("name"));
+            }
+        } catch (SQLException e) {
+            System.out.println("Błąd przy weryfikacji: " + e.getMessage());
+        }
+        return null;
     }
 }
